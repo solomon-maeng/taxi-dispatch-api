3921ff0c54a2eec645087c266a829e3bc208db33
solomon.maeng <solomon.maeng@dramancompany.com>
2022-10-11 18:12:46 +0900

    API-7300 users 모델 누락된 테스트 추가
    - users 모델의 누락된 이메일 형식 검증 테스트를 추가한다.
    - FactoryBot의 user_type 생성과 관련한 잘못된 동작을 수정한다.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.unique.email }
     password_digest { Faker::Internet.password }
-    user_type { %w(passenger, driver).sample }
+    user_type { User.user_types.keys.sample }
   end
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -4,19 +4,25 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  describe '유저 모델이 공백 여부를 검증한다.' do
+  describe '유저 모델의 공백 여부를 검증한다.' do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:user_type) }
     it { should validate_presence_of(:password_digest) }
   end
 
-  describe '유저 모델의 이메일이 유일한 지 검증한다.' do
+  describe '유저 모델의 이메일 속성 유일성을 검증한다.' do
     let!(:user) { create(:user) }
 
     it { should validate_uniqueness_of(:email).case_insensitive }
   end
 
-  describe '유저 모델의 user_type이 존재하는지 검증한다.' do
+  describe '유저 모델의 이메일 형식을 검증한다.' do
+    let(:invalid_email) { 'foobar' }
+
+    it { should_not allow_value(invalid_email).for(:email) }
+  end
+
+  describe '유저 모델의 user_type 일치 여부를 검증한다.' do
     it { should validate_inclusion_of(:user_type).in?(%w(passenger driver)) }
   end
 end
