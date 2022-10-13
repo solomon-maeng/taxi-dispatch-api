01b0e2be9313c647caa7e0c9cf891a792a5fd599
solomon.maeng <solomon.maeng@dramancompany.com>
2022-10-13 15:45:39 +0900

    API-7329 taxi_requests 테이블 추가
    - taxi_requests 테이블을 추가한다.

--- db/structure.sql
+++ db/structure.sql
@@ -14,3 +14,21 @@ CREATE TABLE `users`
     PRIMARY KEY (`id`),
     UNIQUE KEY `unique_idx_email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `taxi_requests`;
+CREATE TABLE `taxi_requests`
+(
+    `id`           bigint       NOT NULL AUTO_INCREMENT,
+    `driver_id`    bigint       NOT NULL,
+    `passenger_id` bigint       NOT NULL,
+    `address`      varchar(255) NOT NULL,
+    `status`       enum('pending', 'accepted') NOT NULL,
+    `accepted_at`  datetime(6),
+    `created_at`   datetime(6),
+    `updated_at`   datetime(6),
+    PRIMARY KEY (`id`),
+    KEY            `idx_created_at` (`created_at`),
+    KEY            `idx_accepted_at` (`accepted_at`),
+    KEY            `idx_driver_id` (`driver_id`),
+    KEY            `idx_passenger_id` (`passenger_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
