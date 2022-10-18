05f7325530ef5d28e2ef9b3fe93cefd58f13cd2d
solomon.maeng <solomon.maeng@dramancompany.com>
2022-10-18 17:51:39 +0900

    API-7353 배차 요청 수락 API 구현 및 검증
    - 배차 요청 수락 API 구현
    - 인증 정보가 유효하지 않은 경우에 대한 검증 테스트 추가
    - 기사가 아닌 유저가 배차 요청을 수락하는 경우에 대한 검증 테스트 추가
    - 요청한 배차 정보가 존재하지 않는 경우에 대한 검증 테스트 추가
    - 이미 수락된 배차 요청인 경우에 대한 검증 테스트 추가

--- app/models/taxi_request.rb
+++ app/models/taxi_request.rb
@@ -7,4 +7,8 @@ class TaxiRequest < ApplicationRecord
   validates :address, length: { maximum: 100 }, presence: true
   validates :status, inclusion: { in: TaxiRequest.statuses.keys }
   validates :driver_id, :passenger_id, presence: true
+
+  def accepted_by(user)
+    update!(driver_id: user.id, status: :accepted, accepted_at: Time.current)
+  end
 end
